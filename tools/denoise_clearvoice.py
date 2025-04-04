bb13de3ffba1ff39e83153e99fb826ea91f62b3c
zach_autodl <zachx121@outlook.com>
2025-04-04 17:18:07 +0800

    fix | GSV_model | 使用ASR兜底ClearVoice降噪异常(人声噪音互换)

--- tools/denoise_clearvoice.py
+++ tools/denoise_clearvoice.py
@@ -3,32 +3,83 @@ import sys
 import os
 import subprocess
 from glob import glob
-# from tqdm import tqdm
+from tqdm.auto import tqdm
 import logging
+import torch
+from faster_whisper import WhisperModel
 from clearvoice import ClearVoice
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     format='[%(asctime)s-%(levelname)s]: %(message)s',
                     datefmt="%Y-%m-%d %H:%M:%S")
 
+PROJ_DIR = "/root/GPT-SoVITS"
+# python
 if __name__ == '__main__':
     inp_dir = sys.argv[1]  # inp_dir="/root/autodl-fs/voice_sample/lydia/slicer_opt"
     opt_dir = sys.argv[2]  # opt_dir="/root/autodl-fs/voice_sample/lydia/denoise_opt_dev"
     myClearVoice = ClearVoice(task='speech_separation',
                               model_names=['MossFormer2_SS_16K'])
     # 处理目录 | online_write 处理一个音频写入一个音频
+    logging.info(">>> ClearVoice降噪中...")
     myClearVoice(input_path=inp_dir,
                  output_path=opt_dir,
                  online_write=True)
-    # 格式化
-    reg_fp = os.path.join(opt_dir, "MossFormer2_SS_16K", "*_s1.wav")
-    fn_list = glob(reg_fp)
-    for idx, fn in enumerate(fn_list):
-        logging.info(f"Processing {idx/len(fn_list)} ...")
+
+    # 对s1、s2每个音频都调用ASR，哪个得分更高就用哪个，避免有时候s1、s2出现噪音、人声反过来的情况
+    logging.info(">>> 加载ASR模型兜底人声、噪音的s1、s2变量错位的问题...")
+    dir_whisper = os.path.join(PROJ_DIR, 'tools/asr/models/faster-whisper-large-v3-local')
+    assert os.path.exists(dir_whisper)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # 直接下载的目录还无法加载，需要根据配置指定到哪一个blobl
+    with open(os.path.join(dir_whisper, "refs", "main"), "r") as fr:
+        _version = fr.readlines()[0]
+        fp_whisper = os.path.join(dir_whisper, "snapshots", _version)
+        assert os.path.exists(fp_whisper)
+    model = WhisperModel(fp_whisper, device=device, compute_type="float32")
+    denoise_dir = os.path.join(opt_dir, "MossFormer2_SS_16K")
+    wav_fp_list = glob(os.path.join(denoise_dir, "*s1.wav"))
+    res_fp_list = []
+    for s1_fp in tqdm(wav_fp_list):
+        s2_fp = s1_fp.replace("s1.wav", "s2.wav")
+        assert os.path.exists(s1_fp)
+        assert os.path.exists(s2_fp)
+        segments, info = model.transcribe(
+            audio=s1_fp,
+            beam_size=5,
+            # vad_filter=True,
+            # vad_parameters=dict(min_silence_duration_ms=700),
+            language=None)
+        s1_lang_prob = info.language_probability
+        s1_avg_logprob = [seg.avg_logprob for seg in segments][0]  # 这个list里所有的avg_logprob是一样的
+
+        segments, info = model.transcribe(
+            audio=s2_fp,
+            beam_size=5,
+            # vad_filter=True,
+            # vad_parameters=dict(min_silence_duration_ms=700),
+            language=None)
+        s2_lang_prob = info.language_probability
+        s2_avg_logprob = [seg.avg_logprob for seg in segments][0]  # 这个list里所有的avg_logprob是一样的
+
+        if s1_avg_logprob > s2_avg_logprob:
+            res_fp_list.append(s1_fp)
+        else:
+            print(f">>> detect unexpected denoise result, add s2 as final result (s2: {s2_fp})")
+            res_fp_list.append(s2_fp)
+
+        # print(f"lang_prob: {'s1' if s1_lang_prob>s2_lang_prob else 's2'} s1={s1_lang_prob} s2={s2_lang_prob}")
+        # print(f"logprob: {'s1' if s1_avg_logprob>s2_avg_logprob else 's2'} s1={s1_avg_logprob} s2={s2_avg_logprob}")
+        # Audio(s1_fp)
+        # Audio(s2_fp)
+
+    logging.info(">>> 将ASR分数更高的文件mv到目标目录")
+    for fn in tqdm(res_fp_list):
         res = subprocess.run(["mv", fn, opt_dir],
                              capture_output=True, text=True,
                              encoding='utf-8')
         assert res.returncode == 0
 
+    logging.info(">>> 清理临时目录")
     res = subprocess.run(["rm", "-rf", os.path.join(opt_dir, "MossFormer2_SS_16K")],
                          capture_output=True, text=True,
                          encoding='utf-8')
@@ -40,4 +91,6 @@ if __name__ == '__main__':
     # output_wav = myClearVoice(input_path=input_path, online_write=False)
     # # output_wav[0]是声音，[1]是底噪
     # myClearVoice.write(output_wav, output_path=opt_path)
+
+
 
