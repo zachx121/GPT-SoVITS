3c0c5aed5606dafd9f285646e8c765cbf1ff37b0
zach <zachx121@outlook.com>
2025-04-21 19:24:29 +0800

    denoise | 差距更大时才使用s2 & 兜底seg为空

--- tools/denoise_clearvoice.py
+++ tools/denoise_clearvoice.py
@@ -9,14 +9,17 @@ import torch
 from faster_whisper import WhisperModel
 from clearvoice import ClearVoice
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
-                    format='[%(asctime)s-%(levelname)s]: %(message)s',
+                    format='[%(asctime)s-%(levelname)s-%(name)s]: %(message)s',
                     datefmt="%Y-%m-%d %H:%M:%S")
+logging.getLogger("faster_whisper").setLevel(logging.ERROR)
 
 PROJ_DIR = "/root/GPT-SoVITS"
-# python
+# python denoise_clearvoice_dev.py /root/autodl-fs/voice_sample/ChatTTS_Voice_Clone_User_3870_20250421164911734_aiqs/sliced /root/autodl-fs/voice_sample/ChatTTS_Voice_Clone_User_3870_20250421164911734_aiqs/denoised
 if __name__ == '__main__':
-    inp_dir = sys.argv[1]  # inp_dir="/root/autodl-fs/voice_sample/lydia/slicer_opt"
-    opt_dir = sys.argv[2]  # opt_dir="/root/autodl-fs/voice_sample/lydia/denoise_opt_dev"
+    inp_dir = sys.argv[1]  # inp_dir="/root/autodl-fs/voice_sample/ChatTTS_Voice_Clone_User_3870_20250421164911734_aiqs/sliced"
+    opt_dir = sys.argv[2]  # opt_dir="/root/autodl-fs/voice_sample/ChatTTS_Voice_Clone_User_3870_20250421164911734_aiqs/denoised_dev"
+    logging.info(f"Denoise inp: {inp_dir}")
+    logging.info(f"Denoise opt: {opt_dir}")
     myClearVoice = ClearVoice(task='speech_separation',
                               model_names=['MossFormer2_SS_16K'])
     # 处理目录 | online_write 处理一个音频写入一个音频
@@ -36,10 +39,11 @@ if __name__ == '__main__':
         fp_whisper = os.path.join(dir_whisper, "snapshots", _version)
         assert os.path.exists(fp_whisper)
     model = WhisperModel(fp_whisper, device=device, compute_type="float32")
+    # 对每一个denoise的s1、s2使用whisper进行文本推理
     denoise_dir = os.path.join(opt_dir, "MossFormer2_SS_16K")
     wav_fp_list = glob(os.path.join(denoise_dir, "*s1.wav"))
     res_fp_list = []
-    for s1_fp in tqdm(wav_fp_list):
+    for s1_fp in wav_fp_list:
         s2_fp = s1_fp.replace("s1.wav", "s2.wav")
         assert os.path.exists(s1_fp)
         assert os.path.exists(s2_fp)
@@ -50,7 +54,10 @@ if __name__ == '__main__':
             # vad_parameters=dict(min_silence_duration_ms=700),
             language=None)
         s1_lang_prob = info.language_probability
-        s1_avg_logprob = [seg.avg_logprob for seg in segments][0]  # 这个list里所有的avg_logprob是一样的
+        segments1 = list(segments)
+        # 这个list里所有的avg_logprob是一样的
+        s1_avg_logprob = segments1[0].avg_logprob if len(segments1) >= 1 else -999
+        s1_nospeech_prob = segments1[0].no_speech_prob if len(segments1) >= 1 else -999
 
         segments, info = model.transcribe(
             audio=s2_fp,
@@ -59,18 +66,18 @@ if __name__ == '__main__':
             # vad_parameters=dict(min_silence_duration_ms=700),
             language=None)
         s2_lang_prob = info.language_probability
-        s2_avg_logprob = [seg.avg_logprob for seg in segments][0]  # 这个list里所有的avg_logprob是一样的
+        segments2 = list(segments)
+        # 这个list里所有的avg_logprob是一样的
+        s2_avg_logprob = segments2[0].avg_logprob if len(segments2) >= 1 else -999
+        s2_nospeech_prob = segments2[0].no_speech_prob if len(segments2) >= 1 else -999
 
-        if s1_avg_logprob > s2_avg_logprob:
-            res_fp_list.append(s1_fp)
-        else:
-            print(f">>> detect unexpected denoise result, add s2 as final result (s2: {s2_fp})")
+        if s2_avg_logprob - s1_avg_logprob > 0.025:
+            logging.info(f"\n>>> detect unexpected denoise result (gap: {s2_avg_logprob - s1_avg_logprob:.4f}), use s2 {s2_fp})")
+            logging.info(f"s1_lang_prob:{s1_lang_prob}, s1_avg_logprob:{s1_avg_logprob}, s1_nospeech_prob:{s1_nospeech_prob} \nseg1:{segments1}")
+            logging.info(f"s2_lang_prob:{s2_lang_prob}, s2_avg_logprob:{s2_avg_logprob}, s2_nospeech_prob:{s2_nospeech_prob} \nseg2:{segments2}")
             res_fp_list.append(s2_fp)
-
-        # print(f"lang_prob: {'s1' if s1_lang_prob>s2_lang_prob else 's2'} s1={s1_lang_prob} s2={s2_lang_prob}")
-        # print(f"logprob: {'s1' if s1_avg_logprob>s2_avg_logprob else 's2'} s1={s1_avg_logprob} s2={s2_avg_logprob}")
-        # Audio(s1_fp)
-        # Audio(s2_fp)
+        else:
+            res_fp_list.append(s1_fp)
 
     logging.info(">>> 将ASR分数更高的文件mv到目标目录")
     for fn in tqdm(res_fp_list):
